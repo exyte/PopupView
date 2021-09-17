7008767a7bc79cf98237ac41999adc6c1ba66bef
Daniil Manin <manindaniil@gmail.com>
2021-09-17 13:05:30 +0700

    Update examples for macos/tvos

--- Example/Common/Utils.swift
+++ Example/Common/Utils.swift
@@ -32,20 +32,24 @@ extension View {
         }
     }
 
+    #if os(iOS)
     func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
         clipShape(RoundedCorner(radius: radius, corners: corners))
     }
+    #endif
 }
 
+#if os(iOS)
 struct RoundedCorner: Shape {
     var radius: CGFloat = .infinity
     var corners: UIRectCorner = .allCorners
-
+    
     func path(in rect: CGRect) -> Path {
         let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
         return Path(path.cgPath)
     }
 }
+#endif
 
 class Constants {
 
