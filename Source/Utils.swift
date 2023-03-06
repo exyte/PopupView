8f63e7fb4c7bde411439d34ce1f2aa368cc10e30
Yona Harel <yonah@balink.net>
2023-03-07 01:47:11 +0200

    removed unnecessary code

--- Source/Utils.swift
+++ Source/Utils.swift
@@ -286,38 +286,3 @@ private struct FullScreenCoverBackgroundRemovalView: UIViewRepresentable {
     func updateUIView(_ uiView: UIView, context: Context) {}
 
 }
-struct AdaptsToKeyboard: ViewModifier {
-    @State var currentHeight: CGFloat = 0
-    
-    func body(content: Content) -> some View {
-        GeometryReader { geometry in
-            content
-                .padding(.bottom, self.currentHeight)
-                .onAppear(perform: {
-                    NotificationCenter.Publisher(center: NotificationCenter.default, name: UIResponder.keyboardWillShowNotification)
-                        .merge(with: NotificationCenter.Publisher(center: NotificationCenter.default, name: UIResponder.keyboardWillChangeFrameNotification))
-                        .compactMap { notification in
-                            withAnimation(.easeOut(duration: 0.16)) {
-                                notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
-                            }
-                    }
-                    .map { rect in
-                        rect.height - geometry.safeAreaInsets.bottom
-                    }
-                    .subscribe(Subscribers.Assign(object: self, keyPath: \.currentHeight))
-                    
-                    NotificationCenter.Publisher(center: NotificationCenter.default, name: UIResponder.keyboardWillHideNotification)
-                        .compactMap { notification in
-                            CGFloat.zero
-                    }
-                    .subscribe(Subscribers.Assign(object: self, keyPath: \.currentHeight))
-                })
-        }
-    }
-}
-
-extension View {
-    func adaptsToKeyboard() -> some View {
-        return modifier(AdaptsToKeyboard())
-    }
-}
